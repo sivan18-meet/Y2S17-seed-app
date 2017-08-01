a677ebdfc311f3b0b5dcc7b2ab9fde5a848a0890
sivan18-meet <sbmislovely@gmail.com>
2017-08-01 17:08:39 +0300

    add display and post posts to the db

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import sys
+
 # flask imports
 from flask import Flask, render_template, request, redirect, url_for
 
 # SQLAlchemy
-from model import Base, Post
-from model import Base, Comment
+from model import Base, Post, Comment
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -15,13 +16,11 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
-
 @app.route('/')
 def my_feed():
-	return render_template('index.html')
+	posts = session.query(Post).all()
+	return render_template('index.html', posts=posts)
+
 
 @app.route('/comment')
 def comment():
@@ -29,5 +28,21 @@ def comment():
 
 @app.route('/<string:button_name>')
 def category(button_name):
-	category_posts= session.query(Post).filter_by(category=button_name).all()
-	return render_template('category.html', button_name=button_name)
+    category_posts= session.query(Post).filter_by(category=button_name).all()
+    return render_template('category.html', button_name=button_name)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    if request.method == "GET":
+        return render_template('post.html')
+    else:
+        new_title    = request.form.get('title')
+        new_category = request.form.get('category')
+        print('This error output %s' % new_title,  file=sys.stderr)
+
+        post = Post(title=new_title, category="eee")
+        session.add(post)
+        session.commit()
+        # ADD SQL SESSION
+        return redirect(url_for('my_feed'))
